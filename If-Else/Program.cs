737ca0892a9469b57dc97f515282549fc759fbd2
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-18 01:25:07 +0300

    If-Else Update

---
+++ If-Else/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace If_Else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool condition = true;
+
+            if (condition)
+            {
+                Console.WriteLine("Değişken: true");
+            }
+            else
+            {
+                Console.WriteLine("Değişken: false");
+            }
+        }
+    }
+}
