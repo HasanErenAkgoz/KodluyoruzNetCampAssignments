41f277d4502f10e743437d19031c5cf44a303c18
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-18 01:23:35 +0300

    Switch-Case Update

---
+++ Switch-Case/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Switch_Case
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (degisken)
+            {
+                case sabit1:
+                    komutlar;
+                    break;
+                case sabit2:
+                    komutlar;
+                    break;
+                case sabit3:
+                    komutlar;
+                    break;
+                default:
+                    komutlar;
+                    break;
+            }
+        }
+    }
+}
