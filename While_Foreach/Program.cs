39ed3ba24456059b8e45f580c0a59a3e3b1dca02
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-19 15:34:53 +0300

    While Update

---
+++ While_Foreach/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace While_Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sayı giriniz :");
+            int a = int.Parse(Console.ReadLine());
+            int b = 1;
+            int c = 0;
+            while (b <= a)
+            {
+                c += b;
+                b++;
+            }
+            Console.WriteLine(c / a);
+            Console.WriteLine("--------------------------");
+            char e = 'a';
+            while (e < 'z')
+            {
+                Console.WriteLine(e);
+                e++;
+
+            }
+            Console.WriteLine("--------------------------");
+            string[] ogrenciler = { "Ali", "Ahmet", "Ayşe", "Aylin" };
+
+            foreach (var i in ogrenciler)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
