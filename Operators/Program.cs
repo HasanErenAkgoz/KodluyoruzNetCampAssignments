dcb5cc8558352dd10725fc2e865284ada919a36e
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-28 01:24:11 +0300

    Methot Update

--- Operators/Program.cs
+++ Operators/Program.cs
@@ -11,8 +11,7 @@ namespace Operators
         }
         public static void solve(double meal_cost, int tip_percent, int tax_percent)
         {
-           double tip_percent=meal_cost*tip_percent/100;
-           double tax_percent=meal_cost*tax_percent/100;
+          
            double totalcost=meal_cost+tip_percent+tax_percent;
            System.Console.WriteLine(Math.Round(totalcost));
 
--- Switch-Case/Program.cs
+++ Switch-Case/Program.cs
@@ -6,21 +6,6 @@ namespace Switch_Case
     {
         static void Main(string[] args)
         {
-            switch (degisken)
-            {
-                case sabit1:
-                    komutlar;
-                    break;
-                case sabit2:
-                    komutlar;
-                    break;
-                case sabit3:
-                    komutlar;
-                    break;
-                default:
-                    komutlar;
-                    break;
-            }
         }
     }
 }
