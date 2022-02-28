64e6153fcbd0ac1aac2eb0509f1caa7cd4f0dff8
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-28 19:20:46 +0300

    StaticClass Updte

---
+++ StaticClass/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace StaticClass
+{
+   class Ogrenci
+{
+    public static int OgrenciSayisi = 0;
+    public string Isim;
+    public string Soyisim;
+    public Ogrenci()
+    {
+        OgrenciSayisi++;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Static sınıf üyesine erişim
+        Console.WriteLine("Öğrenci sayısı: {0}",Ogrenci.OgrenciSayisi);
+
+        //Static olmayan sinif üyesine erişim
+        Ogrenci ogrenci1 = new Ogrenci();
+        ogrenci1.Isim = "Deniz";
+        ogrenci1.Soyisim = "Arda";
+        
+        Ogrenci ogrenci2 = new Ogrenci();
+        ogrenci2.Isim = "Ayşe";
+        ogrenci2.Soyisim = "Yılmaz";
+
+        Console.WriteLine("Öğrenci Sayısı: {0}", Ogrenci.OgrenciSayisi);
+    }
+}
+}
