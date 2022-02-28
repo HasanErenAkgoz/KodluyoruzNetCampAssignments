4033251227741591779281ff80ea5493dc3b0cf1
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-28 17:42:50 +0300

    Class Update

---
+++ Class/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calısan calısan1=new Calısan();
+            calısan1.Name="Hasan";
+            calısan1.SurName="Akgöz";
+            calısan1.No=5;
+            calısan1.Departman="Bilgi İşlem";
+            calısan1.CalısanBİlgileri();
+        }
+    }
+    public class Calısan
+    {
+
+        public string Name { get; set; }
+        public string SurName { get; set; }
+        public int No { get; set; }
+        public string Departman { get; set; }
+
+        public void CalısanBİlgileri()
+        {
+            System.Console.WriteLine("Çalışanın Adı {0}", Name);
+            System.Console.WriteLine("Çalışanın Soyadı {0}", SurName);
+            System.Console.WriteLine("Çalışanın Numarası {0}", No);
+            System.Console.WriteLine("Çalışanın Departmanı {0}", Departman);
+
+        }
+
+    }
+}
