004968ab2f43ac3efc6d8da6af427b66951ee384
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-28 17:05:33 +0300

    ArrayList update

---
+++ ArrayList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+namespace ArrayLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //birden fazla veri tipini aynı anda saklayabiliriz.
+            ArrayList arrayList=new ArrayList();
+            arrayList.Add("Eren");
+            arrayList.Add(2);
+            arrayList.Add(true);
+            arrayList.Add('a');
+
+            System.Console.WriteLine(arrayList[1]);
+            foreach (var item in arrayList)
+            {
+                System.Console.WriteLine(item );
+            }
+        }
+    }
+}
