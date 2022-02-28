b7c574c6518fbbd22d229b4d16e4d1490d2ed2c8
Hasan Eren Akgoz <eren34akgoz@gmail.com>
2022-02-28 18:34:47 +0300

    Encapsulation

--- Class/Program.cs
+++ Class/Program.cs
@@ -6,12 +6,19 @@ namespace Class
     {
         static void Main(string[] args)
         {
-            Calısan calısan1=new Calısan();
-            calısan1.Name="Hasan";
-            calısan1.SurName="Akgöz";
-            calısan1.No=5;
-            calısan1.Departman="Bilgi İşlem";
+            Calısan calısan1 = new Calısan();
+            calısan1.Name = "Hasan";
+            calısan1.SurName = "Akgöz";
+            calısan1.No = 5;
+            calısan1.Departman = "Bilgi İşlem";
             calısan1.CalısanBİlgileri();
+            System.Console.WriteLine("******************");
+            Calısan calısan = new Calısan("Eren", "Akgöz", 1, "İnsan Kaynakları");
+            calısan.CalısanBİlgileri();
+            System.Console.WriteLine("******************");
+            Calısan calısan2=new Calısan("Yusuf","Akgöz");
+            calısan2.CalısanBİlgileri();
+
         }
     }
     public class Calısan
@@ -21,6 +28,24 @@ namespace Class
         public string SurName { get; set; }
         public int No { get; set; }
         public string Departman { get; set; }
+
+        public Calısan(string name, string surname, int no, string Departman)
+        {
+            this.Name = name;
+            this.SurName = surname;
+            this.No = no; ;
+            this.Departman = Departman;
+        }
+        public Calısan(string name, string surname)
+        {
+            this.Name = name;
+            this.SurName = surname;
+
+        }
+        public Calısan()
+        {
+
+        }
 
         public void CalısanBİlgileri()
         {
